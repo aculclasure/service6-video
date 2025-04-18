46fb72e9a542cf33bc1a1b11209c7c4f32cc47cb
Andrew Culclasure <elskohder@gmail.com>
2025-04-17 21:36:36 -0400

    commit 11

--- app/sdk/mux/mux.go
+++ app/sdk/mux/mux.go
@@ -3,21 +3,14 @@
 package mux
 
 import (
-	"encoding/json"
 	"net/http"
+
+	"github.com/ardanlabs/service/app/sdk/route/sys/checkapi"
 )
 
 // WebAPI constructs a http.Handler with all application routes bound.
 func WebAPI() *http.ServeMux {
 	mux := http.NewServeMux()
-	h := func(w http.ResponseWriter, r *http.Request) {
-		status := struct {
-			Status string
-		}{
-			Status: "OK",
-		}
-		json.NewEncoder(w).Encode(status)
-	}
-	mux.HandleFunc("GET /test", h)
+	checkapi.Routes(mux)
 	return mux
 }
---
+++ app/sdk/route/sys/checkapi/route.go
@@ -0,0 +1,8 @@
+package checkapi
+
+import "net/http"
+
+func Routes(mux *http.ServeMux) {
+	mux.HandleFunc("GET /v1/liveness", liveness)
+	mux.HandleFunc("GET /v1/readiness", readiness)
+}
